592eae7aa9a5bc2be0e6677d4707e3df17764159
tretsiakova <tretyakova-98@bk.ru>
2021-12-12 20:32:41 +0300

    Добавлены изменения в ПЗ и в прогу

--- project/WebAppMobileRecord/WebAppMobileRecord/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ project/WebAppMobileRecord/WebAppMobileRecord/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace WebAppMobileRecord.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Запомнить меня?")]
             public bool RememberMe { get; set; }
         }
 
